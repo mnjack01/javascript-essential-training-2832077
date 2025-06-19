b1193f3ea4fac31a06f3abadeca6d1699ad682ce
mnjack01 <max.jackson@webflow.com>
2025-06-19 18:45:13 +0000

    up to chapter 6

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,7 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const firstChild = document.querySelector("ul .backpack_features");
+
+firstChild.classList.add("another-class");
